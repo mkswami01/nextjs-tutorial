ec5f9d384c3c0885ae1e08fb57f7529400a6b549
mkumar-alt <manoj@ahelpinghand.care>
2025-01-25 08:37:53 -0700

    Initial push for t deploying and connecting to db

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const customer = () => {
+  return <p>Customers page</p>;
+};
+
+export default customer;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const invoice = () => {
+  return <p>Invoice page</p>;
+};
+
+export default invoice;
